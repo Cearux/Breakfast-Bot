26c431c450cf915a16ce8e0c81a693339b0ba0ec
Cearux <45979831+Cearux@users.noreply.github.com>
2019-08-29 14:39:11 -0400

    Update moderation.py

--- Moderation/moderation.py
+++ Moderation/moderation.py
@@ -17,8 +17,8 @@ class Moderation(Cog):
 		
 		messages = []
 		number = int()
-		channel = ctx.message.channel
-		async for x in channel.logs_from(ctx.message.channel, limit = number):
+		
+		async for x in Client.logs_from(ctx.message.channel, limit = number):
 			messages.append(x)
 		
 		await Client.delete_messages(messages)
