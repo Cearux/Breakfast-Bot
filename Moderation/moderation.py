a4419f885e79410ed7f2840b85de2adf71a9fdaf
Cearux <45979831+Cearux@users.noreply.github.com>
2019-08-29 13:48:03 -0400

    Update moderation.py

--- Moderation/moderation.py
+++ Moderation/moderation.py
@@ -1,1 +1,25 @@
+import discord
+from discord.ext import commands
+from redbot.core import commands
+import asyncio
+import json
+
+class Moderation:
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command()
+	async def purge(ctx, number):
+		"""Mass deletes messages."""
+		
+		messages = []
+		number = int(number)
+		async for x in Client.logs_from(ctx.message.channel, limit = number):
+			messages.append(x)
+		
+		await Client.delete_messages(messages)
+
+
+def setup(bot):
+	bot.add_cog(Moderation(bot))
 
